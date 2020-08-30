ad498fec55d2856c0342d6ba8abd08dd5f4a82ae
ABHAY BHUJANG HEGDE <abhaybhegde@gmail.com>
2020-08-30 05:09:20 -0500

    Add profiles option.Consolidated pom covering Unit and IT

---
+++ src/integration-test/java/aggregator/statements/prepared/SimpleStatementsIT.java
@@ -0,0 +1,14 @@
+package aggregator.statements.prepared;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleStatementsIT {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
