915a5a6659a0d57ff9cf2550be69cd5300aadd2e
ABHAY BHUJANG HEGDE <abhaybhegde@gmail.com>
2020-09-19 12:31:44 -0500

    Moved pom.xml to build_assets/ to avoid building twice in Travis

--- build_with_unit_tests_only.sh
+++ build_with_unit_tests_only.sh
@@ -2,5 +2,5 @@
 
 PROJECT_ROOT=`pwd`
 echo "Currently in $PROJECT_ROOT.."
-mvn clean install -Dskip.integration.tests=true -Dskip.unit.tests=false
+mvn clean install -Dskip.integration.tests=true -Dskip.unit.tests=false -f=build_assets/pom.xml
 
