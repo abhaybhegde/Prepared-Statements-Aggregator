756007c4f6a2153b6d758d9ef3d25543c852a69a
Abhay <kloblivioner@gmail.com>
2020-09-20 21:07:54 +0530

    Update execute_test.sh

--- acceptance_tests/execute_test.sh
+++ acceptance_tests/execute_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "JAVA_HOME:$JAVA_HOME"
-JAR_PATH=../build_assets/target
+JAR_PATH=build_assets/target
 
 java -jar $JAR_PATH/aggregator.statements.prepared-jar-with-dependencies.jar 
 
