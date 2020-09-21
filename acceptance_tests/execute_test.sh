e9e39422effd187a2d89a73422a3c1280846d3f7
ABHAY BHUJANG HEGDE <abhaybhegde@gmail.com>
2020-09-21 10:31:26 -0500

    1.3 Added Minimalistic Acceptance Tests..

--- acceptance_tests/execute_test.sh
+++ acceptance_tests/execute_test.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 echo "JAVA_HOME:$JAVA_HOME"
 JAR_PATH=build_assets/target
+echo "PWD:$PWD"
 
-java -jar $JAR_PATH/aggregator.statements.prepared-jar-with-dependencies.jar 
+
+
+java -jar $JAR_PATH/aggregator.statements.prepared-jar-with-dependencies.jar -input=acceptance_tests/resources/simple_test.log -output=acceptance_tests/output/simple_test.log -package=jboss.jdbc.spy
+
+
 
 
 
